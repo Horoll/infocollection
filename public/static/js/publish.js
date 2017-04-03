4aee594ff2028fda7b8beee1c917e656f072de0d
Horoll <640246255@qq.com>
2017-04-03 11:22:11 +0800

    修改发布任务页面

--- public/static/js/publish.js
+++ public/static/js/publish.js
@@ -12,38 +12,10 @@ $('.timePicker').datetimepicker({
     language: 'zh-CN',
 });
 
-/*表单显示设置*/
-
-/*绑定删除按钮，删除本行*/
-function deleteItem(){
-    var deleteButton=document.getElementsByClassName("remove");
-    for(var i=0;i<deleteButton.length;i++){
-        deleteButton[i].onclick=function () {
-            var parent=this.parentNode.parentNode.parentNode;
-            parent.removeChild(this.parentNode.parentNode);
-        }
-    }
-};
-deleteItem();
 
 
-/*绑定添加按钮，添加一行*/
-var addButton=document.getElementById("add");
 
 
-function add() {
-
-    var addItem=document.getElementsByClassName("item")[0].cloneNode(true);
-    addItem.childNodes[1].childNodes[1].value="";
-    var table=addButton.previousSibling.previousSibling.childNodes[1];
-    table.appendChild(addItem);
-}
-
-
-addButton.onclick=function () {
-    add();
-    deleteItem();
-}
 
 /*表单选择器*/
 //获取表单元素
@@ -53,15 +25,12 @@ function getForms() {
     for(var i=0;i<divs.length;i++){
         forms.push(divs[i]);
     }
-    console.log(forms);
-
 }
 getForms();
 //设定初始状态
 forms[0].style.display="block";
 forms[1].style.display="none";
 forms[2].style.display="none";
-forms[3].style.display="none";
 var chioce=document.getElementById("formType").value;
 
 //绑定选择器
@@ -88,27 +57,31 @@ document.getElementById("formType").onchange=function () {
             forms[0].style.display="none";
             forms[3].style.display="none";
             break;
-        case '4':
-            forms[3].style.display="block";
-            forms[1].style.display="none";
-            forms[2].style.display="none";
-            forms[0].style.display="none";
-            break;
     }
 
 }
 
-/*获取数据转化为json*/
-function getData() {
-    var data=document.getElementsByClassName("item");
-    var dataArray=new Array();
-    for(var i=0;i<data.length;i++){
-        dataArray.push(data[i].childNodes[1].childNodes[1].value);
-    }
-    var dataJson=JSON.stringify(dataArray);
+/*获取任务信息转换为json*/
+/*function getInfo() {
+    //获取元素的值
+    var name=document.getElementsByName("taskname")[0].value;
+    var startTime=document.getElementsByName("start_date")[0].value;
+    var endTime=document.getElementsByName("end_date")[0].value;
+    var taskText=document.getElementsByName("tasktext")[0].innerHTML;
+    var formType=document.getElementById("formType").value;
+
+    var info={};
+    info.taskname=name;
+    info.start_date=startTime;
+    info.end_date=endTime;
+    info.tasktext=taskText;
+    info.form_moudle=formType;
+
+    var infoJSON=JSON.stringify(info);
+
+    //以ajax形式发送
     var jsonhttp=new XMLHttpRequest();
     jsonhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-
     jsonhttp.onreadystatechange=function(){
         if(jsonhttp.readyState==4||jsonhttp.readyState==200){
 
@@ -118,11 +91,13 @@ function getData() {
     jsonhttp.open("POST",url,true);
     jsonhttp.send();
 
-};
+
+}*/
+
 
 /*对前三个表单的非空检查*/
 function checkInput() {
-    if(isEmpty(document.getElementsByName("name")[0].value)==false){
+    if(isEmpty(document.getElementsByName("taskname")[0].value)==false){
         swal({
             title: "任务名称不能为空",
             type: "error",
@@ -131,7 +106,7 @@ function checkInput() {
         });
         return false;
     }
-    else if(isEmpty(document.getElementsByName("startTime")[0].value)==false){
+    else if(isEmpty(document.getElementsByName("start_date")[0].value)==false){
         swal({
             title: "开始时间不能为空",
             type: "error",
@@ -140,7 +115,7 @@ function checkInput() {
         });
         return false;
     }
-    else if(isEmpty(document.getElementsByName("endTime")[0].value)==false){
+    else if(isEmpty(document.getElementsByName("end_date")[0].value)==false){
         swal({
             title: "结束时间不能为空",
             type: "error",
@@ -158,20 +133,11 @@ function checkInput() {
 /*对publish按钮的绑定*/
 document.getElementById("publish").onclick=function(){
 
-    var chioce=document.getElementById("formType").value;
-    var detail=document.getElementById("detail");
-    var formType=document.getElementById("form");
-    console.log((chioce=='1'||chioce=="2"||chioce=='3')&&checkInput());
-    if((chioce=='1'||chioce=="2"||chioce=='3')&&checkInput()==true){
-        detail.submit();
-        formType.submit();
-    }
-    else if(chioce=='4'&&checkInput()==true){
-        detail.submit();
-        getData();
+    var form=document.getElementById("form");
+    if(checkInput()==true){
+        form.submit();
     }
 }
-
 
 
 
