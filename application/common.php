224a592f663b359d063b53b04242d2a13326aeec
Horoll <640246255@qq.com>
2017-04-04 08:41:18 +0800

    在公共函数文件里添加了下载文件函数

--- application/common.php
+++ application/common.php
@@ -10,3 +10,30 @@
 // +----------------------------------------------------------------------
 
 // 应用公共文件
+
+/**
+ * @param $file_url
+ * @param string $new_name
+ * @return string
+ */
+function download($file_url,$new_name=''){
+    if(!isset($file_url)||trim($file_url)==''){
+        return '500';
+    }
+    if(!file_exists($file_url)){ //检查文件是否存在
+        return '404:文件不存在';
+    }
+    $file_name=basename($file_url);
+    $file_type=explode('.',$file_url);
+    $file_type=$file_type[count($file_type)-1];
+    $file_name=trim($new_name=='')?$file_name:urlencode($new_name);
+    $file_type=fopen($file_url,'r'); //打开文件
+    //输入文件标签
+    header("Content-type: application/octet-stream");
+    header("Accept-Ranges: bytes");
+    header("Accept-Length: ".filesize($file_url));
+    header("Content-Disposition: attachment; filename=".$file_name);
+    //输出文件内容
+    echo fread($file_type,filesize($file_url));
+    fclose($file_type);
+}
