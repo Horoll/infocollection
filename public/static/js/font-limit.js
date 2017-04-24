ecbf0894a65e4aaa2a934f55dbe57e3bce2e7165
Horoll <640246255@qq.com>
2017-04-24 21:41:17 +0800

    修改前端

--- public/static/js/font-limit.js
+++ public/static/js/font-limit.js
@@ -2,10 +2,7 @@
  * Created by RaHsu on 2017/4/23.
  */
 var texts=document.querySelector('table').querySelectorAll("tr");
-console.log(texts);
 for(var i=1;i<texts.length;i++){
     var tds=texts[i].querySelectorAll('td');
-    if(tds[1].length>100) {
         tds[1].innerHTML = tds[1].innerHTML.substring(0, 100) + '...';
-    }
 }
